eca1ced640764bbde3cde12b60a3fb3b48aaf5fa
Стас Ладыгин <ladygin_sa_1017@1511.ru>
2022-09-25 03:11:20 +0300

    Solve task 2 in the jet propulsion problem.

---
+++ GetTrajectory.m
@@ -0,0 +1,20 @@
+%Функция, возвращающая траекторию ракеты
+function r = GetTrajectory(start_point, v0, angle, g, k, m)
+    T = 2*v0/g; %время полета ракеты
+    
+    %Решаем систему уравнений
+    [~, rv] = ode45(@(t,rv) [rv(3); rv(4); -k/m*rv(3); -k/m*rv(4)-g], [0, T], [start_point(1), start_point(2), v0*cos(angle), v0*sin(angle)]);
+    
+    %Определение траектории полета ракеты
+    r = rv(:,1:2);
+
+    %Определение точки пересечения траектории с осью OX
+    for i = 1:length(r)-1
+        if r(i,2)*r(i+1,2) < 0
+            %Определение координаты точки пересечения
+            x_cross = r(i,1) + (r(i+1,1)-r(i,1))*abs(r(i,2))/(abs(r(i,2))+abs(r(i+1,2)));
+            r = [r(1:i,:); [x_cross, 0]];
+            break
+        end
+    end
+end
---
+++ JetPropulsion_1.m
@@ -0,0 +1,115 @@
+%Задача попадания ракетой в цель.
+%Найти угол заупска, при котором ракета попадёт в цель с заданым дельта-расстоянием.
+%Реализовать задачу с помощью метода стрельбы.
+
+%Задание параметров
+g = 9.81; %ускорение свободного падения
+k = 0.5; %коэффициент сопротивления воздуха
+m = 1; %масса ракеты
+delta = 5; %дельта-расстояние
+L = 150; %расстояние до цели
+v0 = 102; %начальная скорость
+start_point = [0,0]; %начальная точка
+end_point = [L,0]; %конечная точка
+
+%Решение задачи
+%Система уравнений движения ракеты с учетом сопротивления воздуха и гравитации имеет вид:
+%x''(t) + k/m*x'(t) = 0
+%y''(t) + k/m*y'(t) + g = 0
+%Сведем систему к системе первого порядка:
+%x'(t) = v_x(t)
+%y'(t) = v_y(t)
+%v_x'(t) = -k/m*v_x(t)
+%v_y'(t) = -k/m*v_y(t) - g
+%Начальные условия:
+%x(0) = start_point(1)
+%y(0) = start_point(2)
+%v_x(0) = v0*cos(alpha)
+%v_y(0) = v0*sin(alpha)
+%Используем метод стрельбы найдем угол alpha, при котором ракета попадет в цель с заданым дельта-расстоянием.
+%Для нахождения угла alpha будем использовать метод Ньютона.
+
+%Реализация метода стрельбы
+
+%Зададим начальные значения
+alpha_min = 0;
+alpha_max = pi/2;
+alpha = NaN;
+distance = NaN;
+r = [];
+is_found = false;
+N = 0;
+
+%Пока не найдем точный угол alpha, будем менять его на alpha_step
+while true
+    %Увеличиваем количество итераций
+    N=N+1;
+
+    %Находим две траектории ракеты
+    r1 = GetTrajectory(start_point, v0, (alpha_max+2*alpha_min)/3, g, k, m);
+    r2 = GetTrajectory(start_point, v0, (2*alpha_max+alpha_min)/3, g, k, m);
+
+    %Определение расстояний между конечной точкой и точкой попадания ракеты
+    distance1 = abs(r1(end,1) - end_point(1));
+    distance2 = abs(r2(end,1) - end_point(1));
+
+    %Если расстояние меньше дельта-расстояния, то угол alpha найден
+    if distance1 < delta
+        alpha = (alpha_max+2*alpha_min)/3;
+        distance = distance1;
+        r = r1;
+        is_found = true;
+        break
+    end
+    if distance2 < delta
+        alpha = (2*alpha_max+alpha_min)/3;
+        distance = distance2;
+        r = r2;
+        is_found = true;
+        break
+    end
+
+    %Если оба расстояния больше дельта-расстояния, то меняем границы угла alpha
+    if distance1 < distance2
+        alpha_max = (2*alpha_max+alpha_min)/3;
+    else
+        alpha_min = (alpha_max+2*alpha_min)/3;
+    end
+
+    %Если разница между границами угла alpha меньше заданной точности, то угол alpha не найден
+    if abs(alpha_min - alpha_max) < 0.001
+        alpha = (alpha_max+alpha_min)/2;
+        distance = abs(r1(end,1) - end_point(1));
+        r = r1;
+        is_found = false;
+        break
+    end
+end
+
+%Вывод результатов
+if ~is_found
+    disp('Alpha not found, but the closest values:')
+end
+disp(['Alpha = ', num2str(alpha)])
+disp(['Distance from target = ', num2str(distance)])
+disp(['Number of iterations = ', num2str(N)])
+
+%Рисуем траекторию полета ракеты, но до этого её интерполируем кривой Безье 3-го порядка
+
+%Интерполяция траектории ракеты кривой Безье 3-го порядка
+x_interp = linspace(min(r(:,1)), max(r(:,1)), 1000);
+y_interp = interp1(r(:,1), r(:,2), x_interp, 'spline');
+
+%Рисуем траекторию полета ракеты толщиной 2
+plot(x_interp, y_interp, 'LineWidth', 2)
+hold on
+%Рисуем точку цели в виде зеленого флажка
+plot(end_point(1), end_point(2), 'g^', 'MarkerSize', 10, 'MarkerFaceColor', 'g')
+%Рисуем точку попадания ракеты в виде крестика
+plot(r(end,1), r(end,2), 'kx', 'MarkerSize', 10, 'LineWidth', 2)
+grid on
+%Подписываем оси с помощью LaTeX и задаем размер шрифта 14
+xlabel('$x$, m', 'Interpreter', 'latex', 'FontSize', 14)
+ylabel('$y$, m', 'Interpreter', 'latex', 'FontSize', 14)
+title('Trajectory of the rocket')
+legend('Trajectory', 'Target', 'Hit point', 'Location', 'best')
